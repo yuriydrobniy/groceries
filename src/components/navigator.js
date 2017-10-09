669a088b35cdd9b4470f4628646006fb885dbef1
yuriydrobniy <yuriy.drobniy1@gmail.com>
2017-10-09 12:21:47 +0300

    minor changes

--- src/components/navigator.js
+++ src/components/navigator.js
@@ -8,21 +8,9 @@ import Cart from './cart'
 export const IndexNav = StackNavigator({
   list: {
     screen: List,
-    navigationOptions: {
-      header: null,
-      tabBarIcon: ({ tintColor }) => (
-        <Icon name='list' size={30} color={tintColor}/>
-      )
-    }
   },
   cart: {
     screen: Cart,
-    navigationOptions: {
-      header: null,
-      tabBarIcon: ({ tintColor }) => (
-        <Icon name='shopping-cart' size={30} color={tintColor}/>
-      )
-    }
   }
 }, {
   initialRouteName: 'list',
