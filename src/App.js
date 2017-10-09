940f4499680968252d4c5016567fee4e1c7338a6
yuriydrobniy <yuriy.drobniy1@gmail.com>
2017-10-09 12:23:03 +0300

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import { IndexNav } from './components/navigator'
 import {
-  Platform,
-  StyleSheet,
-  Text,
   View,
   StatusBar
 } from 'react-native';
@@ -18,22 +15,3 @@ export default class App extends Component<{}> {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
